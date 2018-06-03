ea1eac7400e6957587f73921005bf9e76d479271
thekgt <amantripathi@gmail.com>
2018-06-03 23:10:57 +0530

    Adding function to make dictionary out of all text emails. TODO: Make it generic thing. Refactoring will be required.

--- feature/extractor.py
+++ feature/extractor.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3.6
+import os
+from collections import Counter
 
+
+def make_dictionary(root_dir):
+    all_words = []
+    emails = [os.path.join(root_dir, f) for f in os.listdir(root_dir)]
+
+    for mail in emails:
+        with open(mail) as m:
+            for line in m:
+                words = line.split()
+                all_words += words
+    dictionary = Counter(all_words)
+
+    list_to_remove = dictionary.keys()
+
+    for item in list_to_remove:
+        if item.is_alpha() == False:
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+
+    dictionary = dictionary.most_common(3000)
+
+    return dictionary
